bf3764fb0d6667d02a900aeff67d6f97712a34f2
Cedrine Tille <cedrine.tille@gmail.com>
2024-11-29 17:52:17 +0100

    Update links on the index page

--- pages/index.php
+++ pages/index.php
@@ -8,20 +8,19 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page d'accueil</title>
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="/assets/style.css">
 </head>
 
 <body>
     <header>
         <ul>
-            <li><a href="index.php">Verdo Sàrl</a></li>
-            <li><a href="pages/histoire.php">Notre histoire</a></li>
-            <li><a href="pages/monCompte.php">Mon compte</a></li>
+            <li><a href="index.php">Babel</a></li>
+            <li><a href="about.html">A propos</a></li>
             <?php if (isset($_SESSION['utilisateur'])): ?>
-                <li id="deconnexion"><a href="pages/deconnexion.php">Se déconnecter</a></li>
+                <li id="deconnexion"><a href="deconnexion.php">Se déconnecter</a></li>
             <?php else: ?>
-                <li id="connexion"><a href="pages/connexion.php">Se connecter</a></li>
-                <li id="nouveauCompte"><a href="pages/nouveauCompte.php">Créer un compte</a></li>
+                <li id="connexion"><a href="connexion.html">Se connecter</a></li>
+                <li id="nouveauCompte"><a href="inscription.html">Créer un compte</a></li>
             <?php endif; ?>
         </ul>
     </header>
