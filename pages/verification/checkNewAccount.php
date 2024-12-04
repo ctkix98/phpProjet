6c059a3eb0385bfb1170a8bfe255f885f2f6984c
Cedrine Tille <cedrine.tille@gmail.com>
2024-12-04 16:19:07 +0100

    Create new file to test if pseudo is null

--- pages/verification/checkNewAccount.php
+++ pages/verification/checkNewAccount.php
@@ -7,7 +7,7 @@ $message = "";
 
 //Recevoir les données, et vérifier si c'est juste
 if (filter_has_var(INPUT_POST, 'submit')) { //ok ça fonctionne
-    $donneeUtilisateur['pseudo'] = filter_input(INPUT_POST, 'pseudo', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{3,20}$/"]]);
+    $donneeUtilisateur['pseudo'] = filter_input(INPUT_POST, 'pseudo');
     $donneeUtilisateur['email'] = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
     $donneeUtilisateur['password'] = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{8,20}$/"]]);
 } else {
