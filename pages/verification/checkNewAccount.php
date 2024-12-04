be52d89f5ee7536fee5ab80aa5acadc860b3efd6
Cedrine Tille <cedrine.tille@gmail.com>
2024-12-04 16:05:02 +0100

    Fix typo in regex pseudo

--- pages/verification/checkNewAccount.php
+++ pages/verification/checkNewAccount.php
@@ -7,43 +7,36 @@ $message = "";
 
 //Recevoir les données, et vérifier si c'est juste
 if (filter_has_var(INPUT_POST, 'submit')) { //ok ça fonctionne
-    $donneeUtilisateur['nom'] = filter_input(INPUT_POST, 'nom', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-zÇÉÈÊËÀÂÎÏÔÙÛçéèêëàâîïôùû' -]{1,40}$/"]]);
-    $donneeUtilisateur['prenom'] = filter_input(INPUT_POST, 'prenom', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-zÇÉÈÊËÀÂÎÏÔÙÛçéèêëàâîïôùû' -]{1,40}$/"]]);
+    $donneeUtilisateur['pseudo'] = filter_input(INPUT_POST, 'pseudo', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{3,20}$/"]]);
     $donneeUtilisateur['email'] = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-    $donneeUtilisateur['tel'] = filter_input(INPUT_POST, 'tel', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^\d{3}\s?\d{3}\s?\d{2}\s?\d{2}$/"]]);
-    $donneeUtilisateur['mdp'] = filter_input(INPUT_POST, 'mdp', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{8,20}$/"]]);
+    $donneeUtilisateur['password'] = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{8,20}$/"]]);
 } else {
     $message = "Les informations entrées ne sont pas conformes à la demande";
     $_SESSION['message'] = $message;
-    header('Location: ../pages/errorCreationCompte.php', true, 303);
+    header('Location: ../messages/errorMessage.php', true, 303);
     exit();
 }
 
 //Vérifier si tous les champs sont remplis
 $required = [
-    'nom',
-    'prenom',
+    'pseudo',
     'email',
-    'tel',
-    'mdp',
+    'password',
 ];
-$message = "Tous les champs doivent être complétés ! ";
 foreach ($required as $champ) {
     if (empty($donneeUtilisateur[$champ])) {
+        $message = "Le champ ".$champ ."est vide";
         $_SESSION['message'] = $message;
-        header('Location: ../pages/errorCreationCompte.php', true, 303);
+        header('Location: ../messages/errorMessage.php', true, 303);
         exit();
     }
 }
 
 //Traitement des données
-$donneeUtilisateur['nom'] = ucwords(strtolower($donneeUtilisateur['nom']), " -'");
-$donneeUtilisateur['prenom'] = ucwords(strtolower($donneeUtilisateur['prenom']), " -'");
 $donneeUtilisateur['email'] = strtolower($donneeUtilisateur['email']);
-$donneeUtilisateur['tel'] = str_replace(' ', '', $donneeUtilisateur['tel']);
 
 //changer mdp pour un hash
-$donneeUtilisateur['mdp'] = password_hash($donneeUtilisateur['mdp'], PASSWORD_DEFAULT);
+$donneeUtilisateur['password'] = password_hash($donneeUtilisateur['password'], PASSWORD_DEFAULT);
 
 
 //Appel de la DB
@@ -55,11 +48,9 @@ if ($db->creerTablePersonnes()) {
 
 //Création d'une nouvelle personne avec les données enregistrées
 $personne = new Personne(
-    $donneeUtilisateur['prenom'],
-    $donneeUtilisateur['nom'],
+    $donneeUtilisateur['pseudo'],
     $donneeUtilisateur['email'],
-    $donneeUtilisateur['tel'],
-    $donneeUtilisateur['mdp']
+    $donneeUtilisateur['password']
 );
 $id = $db->ajouterPersonne($personne);
 if ($id > 0) {
@@ -69,6 +60,6 @@ if ($id > 0) {
 } else {
     $message = "Le compte n'a pas pu être créé, car le numéro de téléphone ou l'email est déjà utilisé";
     $_SESSION['message'] = $message;
-    header('Location: ../pages/errorCreationCompte.php', true, 303);
+    header('Location: ../messages/errorMessage.php', true, 303);
     exit();
 }
