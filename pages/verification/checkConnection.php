4eef372136743aac42c66022d615830cfd48672e
Cedrine Tille <cedrine.tille@gmail.com>
2024-12-04 14:16:13 +0100

    Update link to redirect if error occurs when connecting

--- pages/verification/checkConnection.php
+++ pages/verification/checkConnection.php
@@ -12,7 +12,7 @@ if (filter_has_var(INPUT_POST, 'submit')) {
     $donneeConnexion['mdp'] = filter_input(INPUT_POST, 'mdp', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{8,20}$/"]]);
 } else {
     $_SESSION['message'] = "Les informations entrées ne sont pas conformes à la demande";
-    header('Location: ../pages/errorConnexion.php', true, 303);
+    header('Location: ../messages/errorMessage.php', true, 303);
     exit();
 }
 
@@ -20,7 +20,7 @@ $required = ['email', 'mdp'];
 foreach ($required as $champ) {
     if (empty($donneeConnexion[$champ])) {
         $_SESSION['message'] = "Tous les champs doivent être complétés ! ";
-        header('Location: ../pages/errorConnexion.php', true, 303);
+        header('Location: ../messages/errorMessage.php', true, 303);
         exit();
     }
 }
@@ -36,17 +36,17 @@ if ($donnesCompletesUtilisateur !== null) {
     if (password_verify($donneeConnexion['mdp'], $donnesCompletesUtilisateur['mdp'])) {
         // Mot de passe correct, établir la session
         $_SESSION['utilisateur'] = $donnesCompletesUtilisateur;
-        header('Location: ../pages/confirmationConnexion.php');
+        header('Location: ../messages/errorMessage.php', true, 303);
         exit();
     } else {
         // Mot de passe incorrect
         $_SESSION['message'] = "Le mot de passe est incorrect";
-        header('Location: ../pages/errorConnexion.php', true, 303);
+        header('Location: ../messages/errorMessage.php', true, 303);
         exit();
     }
 } else {
     // Utilisateur non trouvé
     $_SESSION['message'] = "Le compte avec cet identifiant n'existe pas";
-    header('Location: ../pages/errorConnexion.php', true, 303);
+    header('Location: ../messages/errorMessage.php', true, 303);
     exit();
 }
