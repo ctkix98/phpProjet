520b03fc0babf3a0173d13ab120739d5a5a143d6
Cedrine Tille <cedrine.tille@gmail.com>
2024-12-04 14:55:57 +0100

    Update to access Database

--- pages/verification/checkConnection.php
+++ pages/verification/checkConnection.php
@@ -8,15 +8,16 @@ session_start();
 $donneeConnexion = [];
 
 if (filter_has_var(INPUT_POST, 'submit')) {
-    $donneeConnexion['pseudo'] = filter_input(INPUT_POST, 'pseudo', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{8,20}$/"]]);
-    $donneeConnexion['mdp'] = filter_input(INPUT_POST, 'mdp', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{8,20}$/"]]);
-} else {
+    $donneeConnexion['pseudo'] = filter_input(INPUT_POST, 'pseudo', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{6,20}$/"]]);
+    $donneeConnexion['password'] = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/^[A-Za-z0-9$!€£]{6,20}$/"]]);
+}else {
     $_SESSION['message'] = "Les informations entrées ne sont pas conformes à la demande";
     header('Location: ../messages/errorMessage.php', true, 303);
     exit();
 }
 
-$required = ['pseudo', 'mdp'];
+
+$required = ['pseudo', 'password'];
 foreach ($required as $champ) {
     if (empty($donneeConnexion[$champ])) {
         $_SESSION['message'] = "Tous les champs doivent être complétés ! ";
@@ -32,7 +33,7 @@ $donnesCompletesUtilisateur = $db->verifierAccesEtRecupererUtilisateur($donneeCo
 
 if ($donnesCompletesUtilisateur !== null) {
     // Vérifiez le mot de passe
-    if (password_verify($donneeConnexion['mdp'], $donnesCompletesUtilisateur['mdp'])) {
+    if (password_verify($donneeConnexion['password'], $donnesCompletesUtilisateur['password'])) {
         // Mot de passe correct, établir la session
         $_SESSION['utilisateur'] = $donnesCompletesUtilisateur;
         header('Location: ../messages/okMessage.php', true, 303);
