f8e4128ae097aee9af9b872b172137ef631050a3
RobinChappalley <robinchappalley@gmail.com>
2025-01-03 13:36:28 +0100

    Commented the lines requirering addBooks and fixed the problem

--- db/Database.php
+++ db/Database.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../class/Personne.php';
 require_once __DIR__ . '/../config/db.ini';
-require_once __DIR__ . '/addBook.php';
+//require_once __DIR__ . 'addBooks.php';
 
 
 
@@ -22,7 +22,7 @@ class Database
             error_log("Erreur de connexion : " . $e->getMessage());
             die("Erreur de connexion. Veuillez réessayer plus tard.");
         }
-        addingBooks ();
+       // addingBooks ();
         $this->initialistion();
     }
 
