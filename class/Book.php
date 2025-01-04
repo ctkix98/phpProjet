f6db07342bab708b165e977ebd2dcf9468ecf680
RobinChappalley <robinchappalley@gmail.com>
2025-01-04 21:07:19 +0100

    Update editor to theme, as the editor wasn't provided

--- class/Book.php
+++ class/Book.php
@@ -4,15 +4,35 @@ class Book
 {
     public $title;
     public $author;
-    public $editor;
+    public $theme;
     public $parution_date;
     public $isbn;
-    public function __construct($title, $author, $editor, $parution_date, $isbn)
+    public function __construct($title, $author, $theme, $parution_date, $isbn)
     {
         $this->title = $title;
         $this->author = $author;
-        $this->editor = $editor;
+        $this->theme = $theme;
         $this->parution_date = $parution_date;
         $this->isbn = $isbn;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+    public function getEditor()
+    {
+        return $this->theme;
+    }
+    public function getParutionDate()
+    {
+        return $this->parution_date;
+    }
+    public function getISBN()
+    {
+        return $this->isbn;
     }
 }
