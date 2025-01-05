611adba94fae125f13115994d55b2d10e491a845
auzima <aurelia.zima-tukala@heig-vd.ch>
2025-01-05 20:23:28 +0100

    modification most-read-cover

--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,24 @@
 <?php
 session_start();
+$livres = [
+    ["titre" => "Changer l'eau des fleurs", "image" => "../assets/images/book-cover-most-read/Changer l'eau des fleurs -Valérie Perrin.jpeg"],
+    ["titre" => "Chien blanc", "image" => "../assets/images/book-cover-most-read/Chien blanc - Romain Gary.jpeg"],
+    ["titre" => "L'affaire Alaska Sanders", "image" => "../assets/images/book-cover-most-read/L'affaire Alaska Sanders -Joël Dicker.jpeg"],
+    ["titre" => "La panthère des neiges", "image" => "../assets/images/book-cover-most-read/La panthère des neiges - Sylvain Tesson.jpeg"],
+    ["titre" => "La vie secrète des écrivains", "image" => "../assets/images/book-cover-most-read/La vie secrète des écrivains - Guillaume Musso.jpeg"],
+    ["titre" => "Le bal des folles", "image" => "../assets/images/book-cover-most-read/Le bal des folles-Victoria Mas.jpeg"],
+    ["titre" => "Le consentement", "image" => "../assets/images/book-cover-most-read/Le consentement - Vanessa Springora.jpeg"],
+    ["titre" => "Le Mage du Kremlin", "image" => "../assets/images/book-cover-most-read/Le Mage du Kremlin - Giuliano da Empoli"],
+    ["titre" => "Les Aérostats", "image" => "../assets/images/book-cover-most-read/Les Aérostats-Amélie Nothomb.jpeg"],
+    ["titre" => "un animal sauvage", "image" => "../assets/images/book-cover-most-read/un animal sauvage - joel dicker.jpeg"],
+];
+$filePath = "../assets/images/book-cover-reading/le-petit-prince-antoine-de-saint-exupery.jpeg";
+if (file_exists($filePath)) {
+    echo "L'image existe à l'emplacement : $filePath";
+} else {
+    echo "Image introuvable à l'emplacement : $filePath";
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,48 +46,27 @@ session_start();
         </ul>
     </header>
     <main>
-    <div class="container">
-            <!-- Section des livres les plus lus -->
-            <div class="most-read-books">
-                <h2>Les livres les plus lus</h2>
-                <ul>
-                    <li>
-                        <img src="../assets/images/book-cover-most-read/Changer l'eau des fleurs -Valérie Perrin.jpeg" alt="cover of the book Changer l'eau des fleurs">
-                    </li>
-                    <li>
-                        <img src="../assets/images/book-cover-most-read/Chien blanc - Romain Gary.jpeg" alt="Cover of the book Chien blanc">
-                    </li>
-                    <li>
-                        <img src="../assets/images/book-cover-most-read/L'affaire Alaska Sanders -Joël Dicker.jpeg" alt="Cover of the book L'affaire Alaska Sanders">
-                    </li>
-                    <li>
-                    <img src="../assets/images/book-cover-most-read/La panthère des neiges - Sylvain Tesson.jpeg" alt="Cover of the book La panthère des neiges">
-                    <li>
-                    <img src="../assets/images/book-cover-most-read/La vie secrète des écrivains - Guillaume Musso.jpeg" alt="Cover of the book La vie secrète des écrivains">
-                    </li>
-                    <li>
-                    <img src="../assets/images/book-cover-most-read/Le bal des folles-Victoria Mas.jpeg" alt="Cover of the book Le bal des folles">
-                    </li>
-                    <li>
-                    <img src="../assets/images/book-cover-most-read/Le consentement - Vanessa Springora.jpeg" alt="Cover of the book Le consentement">
-                    </li>
-                    <li>
-                    <img src="../assets/images/book-cover-most-read/Le Mage du Kremlin - Giuliano da Empoli.jpeg" alt="Cover of the book Le mage du Kremlin">
-                    <li>
-                    <img src="../assets/images/book-cover-most-read/Les Aérostats-Amélie Nothomb.jpeg" alt="Cover of the book Les Aérostats">
-                    </li>
-                    <li>
-                    <img src="../assets/images/book-cover-most-read/un animal sauvage - joel dicker.jpeg" alt="Cover of the book Un animal sauvage">
-                    </li>
-                        </ul>
-                    </div>
-    </div>
-        <!-- Contenu principal --> 
         <div class="content">
             <?php if (isset($_SESSION['utilisateur'])): ?>
                 <h1>Bienvenue <?php echo htmlspecialchars($_SESSION['utilisateur']['pseudo']); ?> !</h1>
+                <div class="container">
+                    <!-- Section des livres les plus lus -->
+                    <div class="most-read-books">
+                        <h2>Les 10 livres les plus lus</h2>
+                        <ul>
+                            <?php foreach ($livres as $livre): ?>
+                                <li>
+                                    <img src="<?php echo htmlspecialchars($livre['image']); ?>" alt="Couverture du livre <?php echo htmlspecialchars($livre['titre']); ?>">
+                                    <p><?php echo htmlspecialchars($livre['titre']); ?></p>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </div>
+
+                
             <?php else: ?>
-                <h1>Bienvenue ! </h1>
+                <h1>Bienvenue !</h1>
             <?php endif; ?>
         </div>
     </main>
