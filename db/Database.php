7b25708dfd16fe8f4e82eec5aa4e3d610a42bd8e
RobinChappalley <robinchappalley@gmail.com>
2025-01-07 03:00:25 +0100

    Revert "Correct changes of db"
    This reverts commit f52e7571a915229af9d2bf065008c7b8d60e8780.

--- db/Database.php
+++ db/Database.php
@@ -398,8 +398,8 @@ class Database
     {
         // Récupérer les valeurs des getters et les stocker dans des variables
         $title = $book->getTitle();
-        $author = $book->getAuthor();  // Utilise la méthode getWriter() pour l'auteur
-        $genre = $book->getTheme();   // Utilise la méthode getEditor() pour le genre (ou sinon un champ plus adapté comme getTheme())
+        $author = $book->getWriter();  // Utilise la méthode getWriter() pour l'auteur
+        $genre = $book->getEditor();   // Utilise la méthode getEditor() pour le genre (ou sinon un champ plus adapté comme getTheme())
         $year = $book->getYear();
         $isbn = $book->getIsbn();
         $coverImage = $book->getCoverImagePath(); // Récupérer le chemin de l'image de couverture
@@ -447,21 +447,21 @@ class Database
 
             // Créer une instance de la classe Book
             $books = [
-                new Book("Daphné et le duc", "Anthony Julia Quinn", "Romance", "2021", "9782290254738", "/assets/images/covers/daphne_et_duc.jpeg", 1),
-                new Book("Le cycle de Dune, Tome 1", "Frank Herbert", "Fantasy", "2021", "9782266320542", "/assets/images/covers/cycle_de_dune_tome1.jpeg", 2),
-                new Book("Fascination", "Stephenie Meyer", "Romance", "2011", "9782013212113", "/assets/images/covers/fascination.jpeg", 3),
-                new Book("L'Alchimiste", "Paulo Coelho", "Romance", "2021", "9782290258064", "/assets/images/covers/alchimiste.jpeg", 4),
-                new Book("La panthère des neiges", "Sylvain Tesson", "Fantasy", "2021", "9782072936494", "/assets/images/covers/panthere_des_neiges.jpeg", 5),
-                new Book("Les fiancés de l'hiver", "Christelle Dabos", "Fantasy", "2024", "9782075215466", "/assets/images/covers/fiances_de_lhiver.jpeg", 6),
-                new Book("La vie secrète des écrivains", "Guillaume Musso", "Film", "2020", "9782253237631", "/assets/images/covers/vie_secrete_ecrivains.jpeg", 7),
-                new Book("Les secrets de la femme de ménage", "Freida McFadden", "Film", "2024", "9782290391198", "/assets/images/covers/secret_femme_menage.jpeg", 8),
-                new Book("Le bal des folles", "Victoria Mas", "Film", "2021", "9782253103622", "/assets/images/covers/bal_des_folles.jpeg", 9),
-                new Book("Le silence du rossignol", "Lian Hearn", "Fantasy", "2021", "9782072934902", "/assets/images/covers/silence_rossignol.jpeg", 10),
-                new Book("Le consentement", "Vanessa Springorg", "Science", "2021", "9782253101567", "/assets/images/covers/consentement.jpeg", 11),
-                new Book("Le mage du Kremlin", "Guiliano da Empoli", "Horror", "2024", "9782073003911", "/assets/images/covers/mage_du_kremlin.jpeg", 12),
-                new Book("Le Petit Prince", "Antoine de Saint-Exupéry", "Fantasy", "1999", "9782070408504", "/assets/images/covers/petit_prince.jpeg", 13),
-                new Book("Le messie de Dune", "Frank Herbert", "Fantasy", "2021", "9782221255728", "/assets/images/covers/messie_dune.jpeg", 14),
-                new Book("Les figurants", "Delphine de Vigan", "Science", "2024", "9782073083999", "/assets/images/covers/figurants.jpeg", 15),
+                new Book("Daphné et le duc", "Anthony Julia Quinn", "Romance", "2021", "9782290254738", "/assets/images/covers/daphne_et_duc.jpeg"),
+                new Book("Le cycle de Dune, Tome 1", "Frank Herbert", "Fantasy", "2021", "9782266320542", "/assets/images/covers/cycle_de_dune_tome1.jpeg"),
+                new Book("Fascination", "Stephenie Meyer", "Romance", "2011", "9782013212113", "/assets/images/covers/fascination.jpeg"),
+                new Book("L'Alchimiste", "Paulo Coelho", "Romance", "2021", "9782290258064", "/assets/images/covers/alchimiste.jpeg"),
+                new Book("La panthère des neiges", "Sylvain Tesson", "Fantasy", "2021", "9782072936494", "/assets/images/covers/panthere_des_neiges.jpeg"),
+                new Book("Les fiancés de l'hiver", "Christelle Dabos", "Fantasy", "2024", "9782075215466", "/assets/images/covers/fiances_de_lhiver.jpeg"),
+                new Book("La vie secrète des écrivains", "Guillaume Musso", "Film", "2020", "9782253237631", "/assets/images/covers/vie_secrete_ecrivains.jpeg"),
+                new Book("Les secrets de la femme de ménage", "Freida McFadden", "Film", "2024", "9782290391198", "/assets/images/covers/secret_femme_menage.jpeg"),
+                new Book("Le bal des folles", "Victoria Mas", "Film", "2021", "9782253103622", "/assets/images/covers/bal_des_folles.jpeg"),
+                new Book("Le silence du rossignol", "Lian Hearn", "Fantasy", "2021", "9782072934902", "/assets/images/covers/silence_rossignol.jpeg"),
+                new Book("Le consentement", "Vanessa Springorg", "Science", "2021", "9782253101567", "/assets/images/covers/consentement.jpeg"),
+                new Book("Le mage du Kremlin", "Guiliano da Empoli", "Horror", "2024", "9782073003911", "/assets/images/covers/mage_du_kremlin.jpeg"),
+                new Book("Le Petit Prince", "Antoine de Saint-Exupéry", "Fantasy", "1999", "9782070408504", "/assets/images/covers/petit_prince.jpeg"),
+                new Book("Le messie de Dune", "Frank Herbert", "Fantasy", "2021", "9782221255728", "/assets/images/covers/messie_dune.jpeg"),
+                new Book("Les figurants", "Delphine de Vigan", "Science", "2024", "9782073083999", "/assets/images/covers/figurants.jpeg"),
             ];
 
             foreach ($books as $book) {
@@ -772,7 +772,8 @@ class Database
         }
     }
 
-
+    function getBooksByState($userId, $state)
+    {
     function getBooksByState($userId, $state)
     {
         $sql = "SELECT b.* FROM book b
@@ -811,17 +812,5 @@ class Database
         $stmt->bindParam(':researchedWord', $researchedWord);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function getBookById($id)
-    {
-
-        $query = $this->db->prepare("SELECT * FROM books WHERE id = :id");
-
-        $query->bindParam(':id', $id, PDO::PARAM_INT);
-
-        $query->execute();
-
-        return $query->fetch(PDO::FETCH_ASSOC);
     }
 }
